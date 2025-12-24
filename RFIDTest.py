0396494cda004671ae1add0ede86ab6a6144646e
PauseEric <pause.ericl82@gmail.com>
2025-12-24 15:42:56 +0800

    Added GPIO

--- RFIDTest.py
+++ RFIDTest.py
@@ -1,7 +1,20 @@
 from time import sleep
 import sys
+import Jetson.GPIO as GPIO
 from Jetson_MFRC522 import SimpleMFRC522
 reader = SimpleMFRC522()
+
+'''
+This script reads RFID tags using the MFRC522 reader.
+MFRC522 should follow the connection below
+SDA  - Pin 24 (GPIO8)
+SCK  - Pin 23 (GPIO11)
+MOSI - Pin 19 (GPIO10)
+MISO - Pin 21 (GPIO9)
+GND  - (Ground)
+RST  - Pin 22 (GPIO25)
+3.3V - (3.3V)
+'''
 
 try:
     while True:
