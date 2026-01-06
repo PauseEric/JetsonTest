3aa7554b2f04280cc16bb4d452d76ae00bba4776
PauseEric <pause.ericl82@gmail.com>
2026-01-06 16:49:05 +0800

    debug

--- JetsonOrinNano/demoTest.py
+++ JetsonOrinNano/demoTest.py
@@ -122,7 +122,8 @@ def colorChange(pixel, NUM_PIXELS, status): #status refers to which mode LED is 
 def main():
     print("Main initated, program running...")
     win.show() #diplay window
-    
+    time.sleep(2)#wait for UI to load
+
     #receivedString = ""
     
 
